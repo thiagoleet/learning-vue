73a5e1c5b43f4e094896f69587fdfb2c3fb9b7bf
Thiago Leite <thiagofmleite@gmail.com>
2018-04-24 21:13:48 -0300

    Componetizando os elementos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
+
+
 <template>
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto in fotos" :key="foto.titulo">
-        <img :src="foto.url" :alt="foto.titulo">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
+
 export default {
+  components: {
+    "meu-painel": Painel
+  },
   data() {
     return {
       titulo: "Alurapic",

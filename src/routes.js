01459c712a11069f9afd57d3841b2d80835a79d6
Thiago Leite <thiago.f.leite@accenture.com>
2018-04-27 12:12:32 -0300

    Rotas nomeadas

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,23 @@
 import Cadastro from "./components/cadastro/Cadastro.vue";
 import Home from "./components/home/Home.vue";
 
-export const routes = [
-  { path: "", component: Home, titulo: "Home" },
-  { path: "/cadastro", component: Cadastro, titulo: "Cadastro" }
+export const routes = [{
+    path: "",
+    name: 'home',
+    component: Home,
+    titulo: "Home",
+    menu: true
+  },
+  {
+    path: "/cadastro",
+    name: 'cadastro',
+    component: Cadastro,
+    titulo: "Cadastro",
+    menu: true
+  },
+  {
+    path: '*',
+    component: Home,
+    menu: false
+  }
 ];

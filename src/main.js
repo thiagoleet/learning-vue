3e6456de01b4455bee30f32fbfd09d22a5c09f33
Thiago Leite <thiagofmleite@gmail.com>
2018-04-29 11:38:39 -0300

    Importando estilos e fontes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import VueResource from "vue-resource";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import App from './App.vue';
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 import {
   routes
-} from "./routes";
+} from './routes';
 import './directives/Transform';
 import VeeValidate from 'vee-validate';
 import msg from './pt_BR';
+import 'bootstrap/dist/css/bootstrap.css';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';

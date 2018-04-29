4f90565305dccb2fd5870454ccb9c4fa68faeb0c
Thiago Leite <thiagofmleite@gmail.com>
2018-04-29 12:00:16 -0300

    Usando plugins JS externos (aka JQuery)

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import './directives/Transform';
 import VeeValidate from 'vee-validate';
 import msg from './pt_BR';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
